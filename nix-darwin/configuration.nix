4ff746d20c73479c4ea3c084d57102cd93e2f10e
Alex-Tudor Petrean <alex-tudor.petrean@snyk.io>
2024-04-23 16:42:58 +0300

    cleanup

--- nix-darwin/configuration.nix
+++ nix-darwin/configuration.nix
@@ -1,15 +1,21 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    home-manager
-    kitty
-    slack
-    dbeaver
-    pinentry_mac
-  ];
+{ pkgs, ... }: {
+  environment.systemPackages =
+    (with pkgs; [
+      home-manager
+      slack
+      dbeaver
+      pinentry_mac
+    ])
+    ++ (with pkgs.darwin; [ cf-private ]);
 
   services.nix-daemon.enable = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs = {
+    hostPlatform = "aarch64-darwin";
+    config.allowUnfree = true;
+  };
 
   programs.zsh.enable = true;
   system.stateVersion = 4;
